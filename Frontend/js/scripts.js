07a508f1ace933a87a3c5d02f063fa8225f4e1e9
Zack Garza <dzackgarza@gmail.com>
2020-05-01 18:34:27 -0400

    / ‘Frontend/js/scripts.js’

--- Frontend/js/scripts.js
+++ Frontend/js/scripts.js
@@ -1,1 +1,10 @@
 // Empty JS for your own code to be here
+
+let parseQuestions = jsonQuestions => {
+  debugger;
+}
+
+fetch("/questions.json")
+  .then(response => response.json())
+  .then(json => parseQuestions(json));
+
