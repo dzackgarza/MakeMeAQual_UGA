b11573fcd253cfbf23c36b81311a07c0161e228e
Zack Garza <dzackgarza@gmail.com>
2020-05-01 19:16:56 -0400

    / ‘Frontend/index.html’ / ‘Frontend/js/scripts.js’

--- Frontend/js/scripts.js
+++ Frontend/js/scripts.js
@@ -1,14 +1,13 @@
-// Empty JS for your own code to be here
 
 let parseQuestions = jsonQuestions => {
   let years = new Set(jsonQuestions.Problems.map(a => a.year));
   years.forEach(j => {
-    $("#selectYears").append($("<option></option>").val(j).html(j));
+    $("#selectYears").append($("<option selected></option>").val(j).html(j));
   });
 
   let topics = new Set([].concat(...jsonQuestions.Problems.map(a => a.tags)))
   topics.forEach(j => {
-    $("#selectTopics").append($("<option></option>").val(j).html(j));
+    $("#selectTopics").append($("<option selected></option>").val(j).html(j));
   });
 
 }
