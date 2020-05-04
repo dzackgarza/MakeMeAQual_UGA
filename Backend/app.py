42a449c2d10aee7b8ea5b9d9fdcfb3268867ff89
Zack Garza <dzackgarza@gmail.com>
2020-05-03 23:55:22 -0400

    / ‘Backend/app.py’

--- Backend/app.py
+++ Backend/app.py
@@ -6,7 +6,11 @@ import subprocess
 app = Flask(__name__, static_folder="Frontend")
 CORS(app)
 
-pandoc_cmd = """
+pandoc_cmd_html = """
+pandoc -f markdown --filter pandoc-include --filter pandoc-theorem-exe -r markdown+tex_math_dollars+simple_tables+table_captions+yaml_metadata_block+smart+blank_before_blockquote+backtick_code_blocks+link_attributes --lua-filter=/home/zack/Notes/Latex/dollar_math.lua -s --mathjax
+"""
+
+pandoc_cmd_pdf = """
 pandoc -f markdown --filter pandoc-include --filter pandoc-theorem-exe -r markdown+tex_math_dollars+simple_tables+table_captions+yaml_metadata_block+smart+blank_before_blockquote+backtick_code_blocks+link_attributes --lua-filter=/home/zack/Notes/Latex/dollar_math.lua -s --mathjax
 """
 
