9fe8a46bb7e00cb379373c1ce710eb8ab37b3962
Zack Garza <dzackgarza@gmail.com>
2020-05-03 23:34:52 -0400

    / ‘Backend/Frontend/index.html’ / ‘Backend/Frontend/js/scripts.js’

--- Backend/Frontend/js/scripts.js
+++ Backend/Frontend/js/scripts.js
@@ -42,6 +42,7 @@ let updateSelectedQuestions = () => {
 $('#makeQual').on('click', function(event) {
   event.preventDefault(); 
   updateSelectedQuestions();
+  let num_questions= parseInt($('numberQuestions').val())
   $.ajax({
     url: 'http://localhost:5000/createqual',
     type: 'post',
@@ -49,7 +50,7 @@ $('#makeQual').on('click', function(event) {
       {
         questions: window.selectedQuestions
           .map(a => a.question)
-          .slice(0, 3),
+          .slice(0, num_questions),
         pdf: 1
       }
     ),
