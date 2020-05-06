5b371274503ba947f2a13dda5027ffa5afebbf0e
Zack Garza <dzackgarza@gmail.com>
2020-05-05 23:58:17 -0400

    / ‘Backend/__pycache__/app.cpython-38.pyc’ / ‘Backend/app.py’

--- Backend/app.py
+++ Backend/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__, static_folder="Frontend")
 CORS(app)
 
 pandoc_cmd_html = """
-pandoc -f markdown --filter pandoc-include --filter pandoc-theorem-exe -r markdown+tex_math_dollars+simple_tables+table_captions+yaml_metadata_block+smart+blank_before_blockquote+backtick_code_blocks+link_attributes --lua-filter=/home/zack/Notes/Latex/dollar_math.lua --template=./webpage_template.html -s --mathjax
+pandoc -f markdown --filter pandoc-include --filter pandoc-theorem-exe -r markdown+tex_math_dollars+simple_tables+table_captions+yaml_metadata_block+smart+blank_before_blockquote+backtick_code_blocks+link_attributes --lua-filter=/home/zack/Notes/Latex/dollar_math.lua -s --mathjax
 """
 
 pandoc_cmd_pdf = """
@@ -20,10 +20,9 @@ def example():
     content = request.get_json()
     questions = content['questions']
     to_pdf = content['do_pdf'] == 1
+    print(to_pdf)
     total_string = ""
-    if(to_pdf):
-        total_string +=  open('./latexmacs.tex', 'r').read() + "\n"
-    pandoc_cmd = pandoc_cmd_pdf if to_pdf else pandoc_cmd_html
+    pandoc_cmd = pandoc_cmd_pdf if to_pdf else pandoc_cmd_html 
     for i, x in enumerate(questions):
         out_str = '# Question {q_number}\n\n{content}\n\n'.format(
                 q_number = i+1, 
@@ -37,13 +36,13 @@ def example():
     p = subprocess.Popen(final_cmd, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p.wait()
+    print(output)
     if (to_pdf):
         response = make_response(bytearray(output))
         response.headers.set('Content-Disposition', 'attachment', filename='qualout.pdf')
         response.headers.set('Content-Type', 'application/pdf')
         return response
     else:
-        print(output)
         return(output)
 
 if __name__ == '__main__':
