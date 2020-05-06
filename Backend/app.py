e92e7dc7c442b13cb16e2abffdc0732dab6eab74
Zack Garza <dzackgarza@gmail.com>
2020-05-05 23:38:52 -0400

    / ‘Backend/__pycache__/app.cpython-38.pyc’ / ‘Backend/app.py’

--- Backend/app.py
+++ Backend/app.py
@@ -21,7 +21,8 @@ def example():
     questions = content['questions']
     to_pdf = content['do_pdf'] == 1
     print("To PDF? ", to_pdf)
-    total_string = "\\newcommand{\ZZ}[0]{{\mathbb{Z}}}\n\n" 
+    total_string =  open('/home/zack/Notes/Latex/latexmacs.tex', 'r').read()
+    # total_string = "\\newcommand{\ZZ}[0]{{\mathbb{Z}}}\n\n" 
     pandoc_cmd = pandoc_cmd_pdf if to_pdf else pandoc_cmd_html
     for i, x in enumerate(questions):
         out_str = '# Question {q_number}\n\n{content}\n\n'.format(
