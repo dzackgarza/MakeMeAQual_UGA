5a05835135f87010907a18b824f9c581831aa644
D. Zack Garza <dzackgarza@gmail.com>
2020-05-09 18:35:24 +0000

    Save

--- Backend/app.py
+++ Backend/app.py
@@ -51,7 +51,7 @@ def example():
         return(output)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', ssl_context="adhoc")
+    app.run(host = '0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
 
 
 
