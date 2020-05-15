515ba16e76335be6fe9bcab3875ccc448d4a8df6
Zack Garza <dzackgarza@gmail.com>
2020-05-14 23:39:14 -0400

    / ‘test.sh’

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-find ./Questions -type f -iname "*.yaml" -exec cat {} + | yq -r '.[] | .question' > test.md            
+find ./Questions -type f -iname "*.yaml" -exec cat {} + | yq -r '.[] | .question' | while read linetext; do
+  echo === $linetext ===
+done
