c9e76711a30d6dd109f8af012851d09ecd0eb4fd
Zack Garza <dzackgarza@gmail.com>
2020-05-14 23:46:23 -0400

    + ‘combined_questions.yaml’ / ‘test.sh’

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 #find ./Questions -type f -iname "*.yaml" -exec cat {} + | yq '.[] | .question' | while read linetext; do
   #echo === $linetext ===
 #done
+
+import yaml
+
+with open(r'combined_questions.yaml') as file:
+    # The FullLoader parameter handles the conversion from YAML
+    # scalar values to Python the dictionary format
+    fruits_list = yaml.load(file, Loader=yaml.FullLoader)
+
+    print(fruits_list)
